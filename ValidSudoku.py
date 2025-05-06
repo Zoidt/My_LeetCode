0bd6e67f900f82e546655e26ec752f0dcb63ff87
Zoidt <zaidtabana@gmail.com>
2025-05-06 15:46:21 -0400

    Update test cases
    also added green colors to passing tests

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -49,12 +49,16 @@ class Solution:
                 curr_square = (row // 3) * 3 + (col // 3)
                 value = board[row][col]
 
+                # Initialize this square if it doesn't exist
+                if curr_square not in square_check:
+                    square_check[curr_square] = {}
+
                 # Check if value is in our square
                 if value in square_check[curr_square]:
                     print("duplicate found in SQUARES")
                     return False
                 else:
-                    square_check[curr_square].append(value)
+                    square_check[curr_square][value] = 1
 
         print("========= Square Check Clear =========")
 
@@ -65,6 +69,7 @@ if __name__ == "__main__":
     
     # ANSI color codes
     RED = '\033[91m'
+    GREEN = '\033[92m'
     RESET = '\033[0m'
     
     # Test Case 1 - Valid Sudoku
@@ -92,17 +97,37 @@ if __name__ == "__main__":
         [".",".",".","4","1","9",".",".","8"],
         [".",".",".",".","8",".",".","7","9"]
     ]
+
+    # Test Case 3 - Invalid Sudoku
+    board3 = [
+        [".",".","4",".",".",".","6","3","."],
+        [".",".",".",".",".",".",".",".","."],
+        ["5",".",".",".",".",".",".","9","."],
+        [".",".",".","5","6",".",".",".","."],
+        ["4",".","3",".",".",".",".",".","1"],
+        [".",".",".","7",".",".",".",".","."],
+        [".",".",".","5",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."]
+    ]
     
     # Test Case 1
     result1 = solution.isValidSudoku(board1)
     if result1 != True:
         print(f"{RED}Test Case 1 Failed! Expected: True, Got: {result1}{RESET}")
     else:
-        print("Test Case 1 Passed! Expected: True, Got:", result1)
+        print(f"{GREEN}Test Case 1 Passed! Expected: True, Got: {result1}{RESET}")
     
     # Test Case 2
     result2 = solution.isValidSudoku(board2)
     if result2 != False:
         print(f"{RED}Test Case 2 Failed! Expected: False, Got: {result2}{RESET}")
     else:
-        print("Test Case 2 Passed! Expected: False, Got:", result2)
+        print(f"{GREEN}Test Case 2 Passed! Expected: False, Got: {result2}{RESET}")
+
+    # Test Case 3
+    result3 = solution.isValidSudoku(board3)
+    if result3 != False:
+        print(f"{RED}Test Case 3 Failed! Expected: False, Got: {result3}{RESET}")
+    else:
+        print(f"{GREEN}Test Case 3 Passed! Expected: False, Got: {result3}{RESET}")
